44660fc72bf1e3ac1d0b2c8fb736200eea06677e
Haniki2k4 <haniki2k4@gmail.com>
2024-03-06 10:13:40 +0700

    <feat>:  add controller NguoiThue

---
+++ Controllers/NguoiThueController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QuanLyTro.Models;
+
+namespace QuanLyTro.Controllers
+{
+    public class NguoiThueController : Controller
+    {
+        // GET: NguoiThue
+        public ActionResult DsachNguoiThue()
+        {
+            QLTTPTEntities db = new QLTTPTEntities();
+            List<TTinNguoiThue> DsNThue = db.TTinNguoiThues.ToList();
+            return View();
+        }
+
+        public ActionResult DKiThue()
+        {
+            return View();
+        }
+    }
+}
